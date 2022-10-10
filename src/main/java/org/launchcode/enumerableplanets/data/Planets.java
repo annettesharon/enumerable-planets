21eef35480e627c029736cbebe69b9b61487628e
annettesharon <annettesharon@ymail.com>
2022-10-10 18:07:44 -0500

    Commit enums

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -0,0 +1,15 @@
+package org.launchcode.enumerableplanets.data;
+
+public enum Planets {
+    MERCURY("Mercury"),
+    VENUS("Venus"), 
+    EARTH("Earth"), 
+    MARS("Mars"), 
+    JUPITER("Jupiter"),
+    SATURN("Saturn"), 
+    URANUS("Uranus"), 
+    NEPTUNE("Neptune");
+
+    Planets() {
+    }
+}
